689fff085b899a54b329db2c7bd6816648242134
Asus Vivobook <dmitriyskrill@gmail.com>
2022-10-15 16:34:51 +0300

    OutputManagement done

---
+++ OutputManagement/webpack.config.js
@@ -0,0 +1,25 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+export default {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+}
